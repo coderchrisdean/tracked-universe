c323f76473b6c6d89507cc08bec4f21ba93cfafe
coderchrisdean <coderchrisdean@gmail.com>
2023-03-29 13:48:12 -0700

    setup server.js with mongo congig

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//initialize express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware
+
+app.use(express.json());
+
+// mongoose configuration
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tracked-universe', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// use routes
+
+app.use(require('.routes'));
+
+// server start
+
+app.listeners(PORT, () => {
+    console.log('server started on port ${PORT}');
+});
